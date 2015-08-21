e2d50e9138df6f43166acd49c6f70146cb301330
MichaelFBiryani <mdfirman@gmail.com>
2015-08-21 10:23:13 +0100

    Gauss filter features, plus also mel spectrogram conversion functions

--- engaged/features/features.py
+++ engaged/features/features.py
@@ -14,9 +14,10 @@ def force_spectrogram_length(spec, desired_length):
 
     desired_length is in bins (not ms)
     """
+    nbins = spec.shape[0]
     if spec.shape[1] < desired_length:
         to_add = desired_length - spec.shape[1]
-        spec = np.hstack((spec, np.zeros((512, to_add))))
+        spec = np.hstack((spec, np.zeros((nbins, to_add))))
     elif spec.shape[1] > desired_length:
         spec = spec[:, :desired_length]
 
@@ -24,8 +25,7 @@ def force_spectrogram_length(spec, desired_length):
 
 
 def small_spectrogram(spec):
-    spec = force_spectrogram_length(spec, 396)
-
+    # spec = force_spectrogram_length(spec, 396)
     im_norm = (spec - spec.mean()) / spec.var()
     return skimage.transform.resize(im_norm, (10, 10)).flatten()
 
@@ -45,7 +45,7 @@ def small_spectrogram_max_pooling(spec):
     return np.max(flatten_view, axis=2).flatten()
 
 
-def frequency_max_pooling(spec, normalise=True):
+def frequency_max_pooling(spec, normalise=True, blocks=True):
     """
     Using code adapated from:
     http://scikit-image.org/docs/dev/auto_examples/plot_view_as_blocks.html
@@ -56,8 +56,11 @@ def frequency_max_pooling(spec, normalise=True):
     else:
         im_norm = spec
 
-    view = view_as_blocks(im_norm, (8, spec.shape[1]))
-    flatten_view = view.reshape(view.shape[0], view.shape[1], -1)
+    if blocks:
+        view = view_as_blocks(im_norm, (8, spec.shape[1]))
+        flatten_view = view.reshape(view.shape[0], view.shape[1], -1)
+    else:
+        flatten_view = spec[None, :, :]
 
     A = np.max(flatten_view, axis=2).flatten()
     B = np.var(flatten_view, axis=2).flatten()
@@ -95,3 +98,104 @@ def max_bins(spec):
     mode_peaks_small = zoom(mode_peaks, 0.10)
 
     return np.hstack((modes_small, mode_peaks_small))
+
+def mel_max_bins(spec):
+    """
+    Computes features from a spectrogram
+    Assumes fixed length
+    """
+
+    # pad the snippit if it is too short, crop if too long
+    num_spectogram_bins = 396
+    spec = force_spectrogram_length(spec, num_spectogram_bins)
+
+    # feature 1: finding the maximum frequency at each time step
+    modes = np.argmax(spec, axis=0)
+    mode_peaks = np.max(spec, axis=0)
+
+    modes_small = zoom(modes, 0.10)
+    mode_peaks_small = zoom(mode_peaks, 0.10)
+
+    return np.hstack((modes_small, mode_peaks_small))
+
+
+from scipy.ndimage.filters import gaussian_filter
+
+def gauss_filters_generator(spec, deviation):
+    """
+    Returns a generator, which gives images of the spectrogram filtered under
+    different gaussians
+    """
+    orders = [[0, 0], [0, 1], [1, 0], [1, 1], [0, 2], [2, 0]]
+
+    for order in orders:
+        yield gaussian_filter(spec, deviation, order=order)
+
+
+def time_max_pooling(array, blockheight):
+    """
+    Does max pooling along the time dimension.
+    Can specify the height of the blocks
+    """
+    assert array.shape[0] % blockheight == 0
+
+    reshaped_array = array.reshape((-1, blockheight * array.shape[1]))
+    return reshaped_array.max(axis=1)
+
+
+def freq_max_pooling(array, blockwidth):
+    """
+    Does max pooling along the frequency dimension.
+    Can specify the width of the blocks
+    """
+    assert array.shape[1] % blockheight == 0
+
+    reshaped_array = array.T.reshape((-1, blockwidth * array.shape[0]))
+    return reshaped_array.max(axis=1)
+
+
+def gauss_filter_max_pooling(spec, deviation, blockheight):
+    """
+    Returns a feature vector, where the spectrogram has been filtered with many
+    different gaussian filters, and max pooling has taken place along the time
+    dimension for different block sizes
+    """
+
+    feature_vectors = []
+
+    for filt in gauss_filters_generator(spec, deviation):
+        feature_vectors.append(time_max_pooling(filt, blockheight))
+
+    return np.hstack(feature_vectors)
+    # return feature_vectors[0]
+
+
+def peak_map(spec, patch_hww=6, rescale_val=0.5):
+    """
+    Returns a patch extracted from the spectrogram around the position of the
+    peak frequency response
+    """
+
+    # resize and pad the spec
+    spec_resized = skimage.transform.rescale(spec, rescale_val)
+    temp_spec = np.pad(spec_resized, patch_hww+1, mode='edge')
+
+    # find the first maximum value. Don't want to find a maximum value on the edges
+    # so we create a new spectrogram
+    max_search_spec = np.pad(spec_resized, patch_hww+1, mode='constant', constant_values=-10000)
+    row, col = np.where(max_search_spec == np.max(max_search_spec))
+    row = row[0]
+    col = col[0]
+
+    # extract the patch
+    patch = temp_spec[(row-patch_hww):(row+patch_hww+1), (col-patch_hww):(col+patch_hww+1)].copy()
+    patch -= patch.mean()
+    patch /= patch.std()
+
+    if patch.shape[0] != 13 or  patch.shape[1] != 13:
+
+        print "Failure!"
+        patch = np.zeros((13, 13))
+
+    return np.hstack((np.ravel(patch), np.max(spec)))
+
--- engaged/features/frequency.py
+++ engaged/features/frequency.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-def spectrogram(wave, sampling_rate, nfft, window_width, overlap, HPF=None, LPF=None, window_type='hamming'):
+def spectrogram(wave, sampling_rate, nfft, window_width, overlap, HPF=None,
+    LPF=None, window_type='hamming', convert_to_mel=False, mel_bins=50,
+    stowell_normalise=False):
     """
     Compute spectrogram and compute log.
 
@@ -33,7 +35,7 @@ def spectrogram(wave, sampling_rate, nfft, window_width, overlap, HPF=None, LPF=
         raise Exception('Window type %s not implemented' % window_type)
 
     # compute fft
-    fft_mat = np.fft.fft(x_wins_ham, n=int(nfft), axis=0)[:(nfft/2), :]
+    fft_mat = np.fft.fft(x_wins_ham, n=int(nfft), axis=0)[:(nfft/2+1), :]
 
     # log magnitude
     fft_mat_lm = np.log(np.abs(fft_mat))
@@ -42,4 +44,77 @@ def spectrogram(wave, sampling_rate, nfft, window_width, overlap, HPF=None, LPF=
         # TODO - high pass and low pass filters here
         raise Exception('Not implemented yet')
 
-    return fft_mat_lm
+    if convert_to_mel:
+        out_spec = spec_to_mel(fft_mat_lm, sampling_rate, mel_bins)
+    else:
+        out_spec = fft_mat_lm
+
+    if stowell_normalise:
+        out_spec -= np.median(out_spec, axis=1)[:, None]
+        out_spec[out_spec<0] = 0
+
+    return out_spec, float(sampling_rate) / float(float(nwin) - 2*float(nstep))
+
+def spec_to_mel(spec, audio_sample_rate, num_mel_bands):
+    """
+    Converts a standard spectrogram to a mel scaled spectrogram
+    """
+    M = mel_binning_matrix(
+        specgram_window_size=(spec.shape[0] - 1) * 2,
+        sample_frequency=audio_sample_rate,
+        num_mel_bands=num_mel_bands)
+
+    return np.dot(M.T, spec)
+
+
+
+# This next bit is from this:
+# https://gist.github.com/benanne/3274371
+def freq2mel(freq):
+    return 1127.01048 * np.log(1 + freq / 700.0)
+
+def mel2freq(mel):
+    return (np.exp(mel / 1127.01048) - 1) * 700
+
+def mel_binning_matrix(specgram_window_size, sample_frequency, num_mel_bands):
+    """
+    function that returns a matrix that converts a regular DFT to a mel-spaced DFT,
+    by binning coefficients.
+
+    specgram_window_size: the window length used to compute the spectrograms
+    sample_frequency: the sample frequency of the input audio
+    num_mel_bands: the number of desired mel bands.
+
+    The output is a matrix with dimensions (specgram_window_size/2 + 1, num_bands)
+    """
+    min_freq, max_freq = 0, sample_frequency / 2
+    min_mel = freq2mel(min_freq)
+    max_mel = freq2mel(max_freq)
+    num_specgram_components = specgram_window_size / 2 + 1
+    m = np.zeros((num_specgram_components, num_mel_bands))
+
+    r = np.arange(num_mel_bands + 2) # there are (num_mel_bands + 2) filter boundaries / centers
+
+    # evenly spaced filter boundaries in the mel domain:
+    mel_filter_boundaries = r * (max_mel - min_mel) / (num_mel_bands + 1) + min_mel
+
+    def coeff(idx, mel): # gets the unnormalised filter coefficient of filter 'idx' for a given mel value.
+        lo, cen, hi = mel_filter_boundaries[idx:idx+3]
+        if mel <= lo or mel >= hi:
+            return 0
+        # linearly interpolate
+        if lo <= mel <= cen:
+            return (mel - lo) / (cen - lo)
+        elif cen <= mel <= hi:
+            return 1 - (mel - cen) / (hi - cen)
+
+
+    for k in xrange(num_specgram_components):
+        # compute mel representation of the given specgram component idx
+        freq = k / float(num_specgram_components) * (sample_frequency / 2)
+        mel = freq2mel(freq)
+        for i in xrange(num_mel_bands):
+            m[k, i] = coeff(i, mel)
+
+    # normalise so that each filter has unit contribution
+    return m / m.sum(0)
