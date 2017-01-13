28a3160c5dcad0c422e7b2715f4a8dbacbdde3e7
MichaelF <mdfirman@gmail.com>
2017-01-13 11:29:42 +0000

    Bugfix to allow to run on Alisons PC

--- golden/train_helpers.py
+++ golden/train_helpers.py
@@ -10,8 +10,12 @@ from lasagne.layers import InputLayer, DimshuffleLayer
 from lasagne.layers import DenseLayer
 from lasagne.layers import NonlinearityLayer
 from lasagne.layers import DropoutLayer
-from lasagne.layers import Pool2DLayer as PoolLayer
-from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
+try:
+    from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
+    from lasagne.layers.dnn import Pool2DDNNLayer as PoolLayer
+except:
+    from lasagne.layers import Conv2DLayer as ConvLayer
+    from lasagne.layers import Pool2DLayer as PoolLayer
 from lasagne.nonlinearities import softmax, very_leaky_rectify as vlr
 import theano
 from lasagne.layers import batch_norm, ElemwiseSumLayer, ExpressionLayer, DimshuffleLayer
