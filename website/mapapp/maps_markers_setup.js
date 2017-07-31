caf51eb1f63a29399e794d8e8c4c775c50a7e6cf
MichaelF <mdfirman@gmail.com>
2017-07-31 18:23:09 +0100

    Better

--- website/mapapp/maps_markers_setup.js
+++ website/mapapp/maps_markers_setup.js
@@ -15,6 +15,8 @@ window.onload = function initMap() {
 
   var london_centre = {lat: 51.529517, lng: -0.058284};
 
+  //$("#myModel").load("./assets/sample_site.html");
+
   // We create the map in the json loader callback, beacuse javascript is strange
   loadJSON('style.json', function(response) {
     loaded_json = JSON.parse(response);
@@ -34,13 +36,13 @@ window.onload = function initMap() {
     map.mapTypes.set('styled_map', styledMapType);
     map.setMapTypeId('styled_map');
 
-    Papa.parse("assets/sites_info.csv", {
+    Papa.parse("assets/sites/sites_info.csv", {
     	download: true,
       dynamicTyping: true,
     	complete: function(results) {
         for (i=0; i<results.data.length - 1; i++)
         {
-          icon_url = './assets/charts/' + results.data[i][0] + '.png'
+          icon_url = './assets/sites/charts/' + results.data[i][0] + '.png'
 
           var marker = new google.maps.Marker({
             position: {lat: results.data[i][1], lng: results.data[i][2]},
@@ -49,8 +51,11 @@ window.onload = function initMap() {
               url: './assets/charts/' + results.data[i][0] + '.png',
               scaledSize: new google.maps.Size(25, 25),
             },
-            icon_path: './assets/charts/' + results.data[i][0] + '.png'
+            icon_path: './assets/charts/' + results.data[i][0] + '.png',
+            postcode: ""+results.data[i][0],
           });
+
+          console.log(results.data[i][0])
 
           google.maps.event.addListener(marker, 'mouseover', function() {
             this.setIcon({
@@ -67,29 +72,33 @@ window.onload = function initMap() {
           });
 
           marker.addListener('click', function() {
+            console.log("Jere")
+            $('#main-modal-title').html(this.postcode)
             $('#myModal').modal('show');
           });
         }
     	}
     });
-
-    var map_inset = new google.maps.Map(document.getElementById('map_inset'), {
-      center: london_centre,
-      zoom: 18,
-      mapTypeId: 'satellite',
-      disableDefaultUI: true,
-      scrollwheel: false,
-      draggable: false
-    });
-
-    // Resize map to show on a Bootstrap's modal
-    $('#myModal').on('shown.bs.modal', function() {
-      var currentCenter = map_inset.getCenter();  // Get current center before resizing
-      google.maps.event.trigger(map_inset, "resize");
-      // console.log(london_centre)
-      map_inset.setCenter(currentCenter); // Re-set previous center
-    });
+    //
+    // var map_inset = new google.maps.Map(document.getElementById('map_inset'), {
+    //   center: london_centre,
+    //   zoom: 18,
+    //   mapTypeId: 'satellite',
+    //   disableDefaultUI: true,
+    //   scrollwheel: false,
+    //   draggable: false
+    // });
+    //
+    // // Resize map to show on a Bootstrap's modal
+    // $('#myModal').on('shown.bs.modal', function() {
+    //   var currentCenter = map_inset.getCenter();  // Get current center before resizing
+    //   google.maps.event.trigger(map_inset, "resize");
+    //   // console.log(london_centre)
+    //   map_inset.setCenter(currentCenter); // Re-set previous center
+    // });
+    //
 
   });
+
 
 }
